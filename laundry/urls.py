892ac6ebe25b76f99bc70615baa5f4818a9fda16
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2024-09-04 19:14:33 +0530

    login added

--- laundry/urls.py
+++ laundry/urls.py
@@ -5,4 +5,6 @@ from laundry import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- laundry/views.py
+++ laundry/views.py
@@ -1,6 +1,35 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "laundry/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "laundry/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
 
 def index(request):
     return render(request, "laundry/index.html")
