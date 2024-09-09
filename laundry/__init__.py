4a97255409668258faf8a001f7e736dd04585f7b
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2024-09-09 23:19:28 +0530

    Remove all previous code and files from repository

--- laundry/__init__.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-MAX_SUBMISSIONS = 4
-MAX_CLOTH_COUNT = 25
-def _md5(number):
-    number_str = str(number).encode()
-    md5_hash = hashlib.md5()
-    md5_hash.update(number_str)
-    return md5_hash.hexdigest()
